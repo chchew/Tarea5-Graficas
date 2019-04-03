6caa8ab80b77e02044cf626777d3b9f1fd4d6761
Carlos Chew <33320933+CarlosChew@users.noreply.github.com>
2019-04-02 18:38:00 -0600

    Se me había olvidado darle commit :(

---
+++ BMP.py
@@ -0,0 +1,114 @@
+#Universidad del Valle de Guatemala
+#CARLOS CHEW - 17507
+
+import struct 
+
+		def __init__(self, width, height):
+		
+			self.width = abs(int(width))
+			self.height = abs(int(height))
+			self.framebuffer = []
+			self.zbuffer = []
+			self.clear()
+			
+		def color(self, r=0, g=0, b=0):
+		
+			if (r > 255 or g > 255 or b > 255 or r < 0 or g < 0 or b <0):
+				r = 0
+				g = 0
+				b = 0
+			return bytes([b, g, r])
+			
+		def clear(self, r=0, b=0, g=0):
+			
+			self.framebuffer = [
+				[
+					self.color(r, b, g)
+						for x in range(self.width)
+				]
+				for y in range(self.height)
+			]
+
+			self.zbuffer = [ [-float('inf') for x in range(self.width)] for y in range(self.height)]
+			
+		def point(self, x, y, color):
+		
+			if(x < self.width and y < self.height):
+				self.framebuffer[x][y] = color
+				
+		def write(self, filename, zbuffer=False):
+		
+			BLACK = self.color(0,0,0)
+			import os
+			os.makedirs(os.path.dirname(filename), exist_ok=True)
+			file = open(filename, "bw")
+			pWidth =  self.__padding(4, self.width)
+			pHeight = self.__padding(4, self.height)
+
+			
+			file.write(self.__char("B"))
+			file.write(self.__char("M")) 
+			file.write(self.___dword(14 + 40 + pWidth * pHeight)) 
+			file.write(self.___dword(0))
+			file.write(self.___dword(14 + 40))
+
+			
+			file.write(self.___dword(40))
+			file.write(self.___dword(self.width))
+			file.write(self.___dword(self.height))
+			file.write(self.___word(1))
+			file.write(self.___word(24))
+			file.write(self.___dword(0))
+			file.write(self.___dword(pWidth * pHeight))
+			file.write(self.___dword(0))
+			file.write(self.___dword(0))
+			file.write(self.___dword(0))
+			file.write(self.___dword(0))
+
+			
+			for x in range(pWidth):
+				for y in range(self.height):
+					if(x < self.width and y < self.height):
+						if zbuffer:
+							if self.zbuffer[y][x] == -float("inf"):
+								file.write(BLACK)
+							else:
+								z = abs(int(self.zbuffer[y][x]*255))
+								file.write(self.color(z,z,z))
+						else:
+							file.write(self.framebuffer[y][x])
+					else:
+						file.write(self.__char("c"))
+
+			file.close()		
+			
+		def __padding(self, base, c):
+			
+			if(c %  base == 0):
+				return c
+			else:
+				while (c % base):
+					c += 1
+				return c	
+				
+		def __char(self, c):
+			return struct.pack("c", c.encode("ascii"))		
+			
+		def ___word(self, c):			
+			return struct.pack("h", c)	
+			
+		def ___dword(self, c):			
+			return struct.pack("l", c)	
+			
+		def getZbufferValue(self, x, y):		
+			if x < self.width and y < self.height:
+				return self.zbuffer[x][y]
+			else:
+				return -float("inf")	
+				
+		def setZbufferValue(self, x, y, z):			
+			if x < self.width and y < self.height:
+				self.zbuffer[x][y] = z
+				return 1
+			else:
+				return 0		
---
+++ OBJ.py
@@ -0,0 +1,150 @@
+#Universidad del Valle de Guatemala
+#CARLOS CHEW - 17507
+
+class OBJ(object):
+	
+
+	def __init__(self, filename):
+		
+		self.__vertex = []
+		self.__faces = []
+		self.__nvertex = []
+		self.__filename = filename
+		self.__materials = None
+		self.__materialFaces = []
+
+	def load(self):
+		
+		file = open(self.__filename, "r")
+		import os
+		faces = []
+		currentMat, previousMat = "default", "default"
+		faceCounter = 0
+		matIndex = []
+		lines = file.readlines()
+		last = lines[-1]
+		for line in lines:
+			line = line.rstrip().split(" ")
+			if line[0] == "mtllib":
+				mtlFile = MTL(os.path.dirname(file.name) + "/" + line[1])
+				if mtlFile.isFileOpened():
+					mtlFile.load()
+					#self.__faces = {}
+					self.__materials = mtlFile.materials
+				else:
+					self.__faces = []
+			elif line[0] == "usemtl":
+				if self.__materials:
+					matIndex.append(faceCounter)
+					previousMat = currentMat
+					currentMat = line[1]
+					if len(matIndex) == 2:
+						self.__materialFaces.append((matIndex, previousMat))
+						matIndex= [matIndex[1]+1]
+			elif line[0] == "v":
+				line.pop(0)
+				i = 1 if line[0] == "" else 0
+				self.__vertex.append((float(line[i]), float(line[i+1]), float(line[i+2])))
+			elif line[0] == "vn":
+				line.pop(0)
+				i = 1 if line[0] == "" else 0
+				self.__nvertex.append((float(line[i]), float(line[i+1]), float(line[i+2])))
+			elif line[0] == "f":
+				line.pop(0)
+				face = []
+				for i in line:
+					i = i.split("/")
+					face.append((int(i[0]), int(i[-1])))
+				self.__faces.append(face)
+				faceCounter += 1
+				face = []
+		if len(matIndex) < 2 and self.__materials:
+			matIndex.append(faceCounter)
+			self.__materialFaces.append((matIndex, currentMat))
+			matIndex= [matIndex[1]+1]
+		file.close()
+
+	def getMaterials(self):
+		
+		return self.__materials
+
+	def getFaceList(self):
+		
+		return self.__faces
+
+	def getVertexList(self):
+	
+		return self.__vertex
+
+	def getVertexNormalList(self):
+	
+		return self.__nvertex
+
+	def getMaterialFaces(self):
+		
+		return self.__materialFaces
+
+class MTL(object):
+	
+
+	def __init__(self, filename):
+		self.__filename = filename
+		self.__file = None
+		self.materials = {}
+		self.readMTLFile()
+
+	def readMTLFile(self):
+	
+		try:
+			self.__file = open(self.__filename, "r")
+			self.__mtlFile = True
+		except:
+			self.__mtlFile = False
+
+	def isFileOpened(self):
+		
+		return self.__mtlFile
+
+	def load(self):
+		
+		if self.isFileOpened():
+			currentMat = None
+			ac, dc, sc, ec, t, s, i, o = 0, 0, 0, 0, 0, 0, 0, 0
+			for line in self.__file.readlines():
+				line = line.split(" ")
+				if line[0] == "newmtl":
+					currentMat = line[1].rstrip()
+				elif line[0] == "Ka":
+					ac = (float(line[1]), float(line[2]), float(line[3]))
+				elif line[0] == "Kd":
+					dc = (float(line[1]), float(line[2]), float(line[3]))
+				elif line[0] == "Ks":
+					sc = (float(line[1]), float(line[2]), float(line[3]))
+				elif line[0] == "Ke":
+					ec = (float(line[1]), float(line[2]), float(line[3]))
+				elif line[0] == "d" or line[0] == "Tr":
+					t = (float(line[1]), line[0])
+				elif line[0] == "Ns":
+					s = float(line[1])
+				elif line[0] == "illum":
+					i  = int(line[1])
+				elif line[0] == "Ni":
+					o = float(line[1])
+				elif currentMat:
+					self.materials[currentMat] = Material(currentMat, ac, dc, sc, ec, t, s, i, o)
+			if currentMat not in self.materials.keys():
+				self.materials[currentMat] = Material(currentMat, ac, dc, sc, ec, t, s, i, o)
+
+class Material(object):
+	
+	def __init__(self, name, ac, dc, sc, ec, t, s, i, o):
+	
+		self.name = name.rstrip()
+		self.ambientColor = ac
+		self.difuseColor = dc
+		self.specularColor = sc
+		self.emissiveCoeficient = ec
+		self.transparency = t
+		self.shininess = s
+		self.illumination = i
+		self.opticalDensity = o
